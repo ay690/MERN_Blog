5928a9172e78554d5ea297474b6133a51145b9b8
Aniket Yadav <aniketyadav690@gmail.com>
2024-08-25 02:23:44 +0530

    update: api url

--- client/src/pages/BlogDetails.jsx
+++ client/src/pages/BlogDetails.jsx
@@ -12,7 +12,7 @@ const BlogDetails = () => {
   // get blog details
   const getBlogDetail = async () => {
     try {
-      const { data } = await axios.get(`/api/v1/blog/get-blog/${id}`);
+      const { data } = await axios.get(`https://mern-blog-yxdo.onrender.com/api/v1/blog/get-blog/${id}`);
       if (data?.success) {
         setBlog(data?.blog);
         setInputs({
@@ -41,7 +41,7 @@ const BlogDetails = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const { data } = await axios.put(`/api/v1/blog/update-blog/${id}`, {
+      const { data } = await axios.put(`https://mern-blog-yxdo.onrender.com/api/v1/blog/update-blog/${id}`, {
         title: inputs.title,
         description: inputs.description,
         image: inputs.image,
--- client/src/pages/Blogs.jsx
+++ client/src/pages/Blogs.jsx
@@ -6,7 +6,7 @@ const Blogs = () => {
   //get blogs
   const getAllBlogs = async () => {
     try {
-      const { data } = await axios.get("/api/v1/blog/all-blog");
+      const { data } = await axios.get("https://mern-blog-yxdo.onrender.com/api/v1/blog/all-blog");
       if (data?.success) {
         setBlogs(data?.blogs);
         console.log(data?.blogs)
